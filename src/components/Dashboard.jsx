23311348bb543436902d27594c1f37d57d816ae0
xhrissun <chrisdanchris@gmail.com>
2025-09-05 20:32:30 +0800

    violet to black candidate details

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -437,7 +437,7 @@ const Dashboard = ({ user, onLogout }) => {
           <img
             src="https://github.com/xhrissun/rhrmpsb-system/blob/main/denr-logo.png?raw=true"
             alt="DENR Logo"
-            className="w-8 h-8 mr-2 object-contain" // Reduced logo size
+            className="w-6 h-6 mr-1 object-contain"
             onError={(e) => {
               e.target.src = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHZpZXdCb3g9IjAgMCA0MCA0MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjQwIiBoZWlnaHQ9IjQwIiBmaWxsPSIjRjNGNEY2Ii8+Cjwvc3ZnPg==';
             }}
@@ -480,7 +480,7 @@ const Dashboard = ({ user, onLogout }) => {
         <div className="mx-6 mt-6 bg-green-50 border border-green-200 rounded-lg p-4">
           <div className="flex">
             <svg className="flex-shrink-0 h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 18a8 8 0 adviser16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
             </svg>
             <div className="ml-3">
               <p className="text-sm text-green-700 font-medium">
