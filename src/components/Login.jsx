5b1e7d31b424e925df45f398614862e1320f48ab
xhrissun <chrisdanchris@gmail.com>
2025-10-07 09:43:13 +0800

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -84,7 +84,7 @@ const Login = React.memo(({ onLogin }) => {
               </picture>
             </div>
             <h1 className="text-2xl sm:text-3xl font-bold text-white">
-              DENR CALABARZON RHRMPSB SYSTEM
+              DENR CALABARZON COMPETENCY-BASED RATING SYSTEM
             </h1>
             <p className="text-sm sm:text-base text-slate-300">
               Sign in to your DENR RHRMPSB account
